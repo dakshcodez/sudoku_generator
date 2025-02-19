cd31565ff0097b68349e5c38c404a658ca8a8b70
dakshcodez <daksharoracodez@gmail.com>
2025-02-19 12:49:45 +0530

    random puzzles are being generated now*

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include<chrono>
 #include "solver.h"
 
+using namespace std;
+
 void shuffleVector(vector<int>& vec){
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
     mt19937 g(seed);
@@ -33,13 +35,40 @@ bool generateFullSudoku(vector<vector<int>>& board,vector<int> vec){
     return true;
 }
 
+void removeNumbers(vector<vector<int>>& board, int difficulty){
+    int emptyCells = difficulty ;
+    while (emptyCells != 0){
+        int r = rand() % 9;
+        int c = rand() % 9;
+        if (board[r][c] != 0){
+            int backup = board[r][c];
+            board[r][c] = 0;
+
+            vector<vector<int>> tempBoard = board;
+            if (solve(tempBoard)){
+                emptyCells--;
+            } else {
+                board[r][c] = backup;
+            }
+        }
+    }
+}
+
 int main(){
+    srand(time(0));
+
     vector<vector<int>> board(9,vector<int>(9,0));
 
     vector<int> num_vec = {1,2,3,4,5,6,7,8,9};
     shuffleVector(num_vec);
 
     generateFullSudoku(board,num_vec);
+    // printBoard(board);
+    // cout<<"\n\n\n";
+    removeNumbers(board, 40);
     printBoard(board);
+    //cout<<"\n\n\n";
+    // solve(board);
+    // printBoard(board);
     return 0;
 }
